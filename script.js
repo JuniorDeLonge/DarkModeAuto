22345159b591f483179bb3d2d9ab2a8009932f8b
Junior DeLonge <147082536+JuniorDeLonge@users.noreply.github.com>
2023-12-12 20:32:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function applyDarkMode() {
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  document.body.classList.toggle("dark-mode", prefersDarkMode);
+class DarkModeHandler {
+  constructor() {
+    this.applyDarkMode = this.applyDarkMode.bind(this);
+  }
+
+  applyDarkMode() {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    document.body.classList.toggle("dark-mode", prefersDarkMode);
+  }
+
+  init() {
+    this.applyDarkMode();
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", this.applyDarkMode);
+  }
 }
 
-applyDarkMode();
-
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", applyDarkMode);
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeHandler = new DarkModeHandler();
+  darkModeHandler.init();
+});
